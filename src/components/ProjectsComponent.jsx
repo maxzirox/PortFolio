0c243f6f8c0dff2b827b9f738b49939bab39d571
Maxi Guzman <32882558+maxzirox@users.noreply.github.com>
2024-09-27 18:36:21 -0300

    update projects

--- src/components/ProjectsComponent.jsx
+++ src/components/ProjectsComponent.jsx
@@ -28,15 +28,27 @@ const ProjectsComponent = () => {
   const projects = [
     {
       title: 'E&M limitada',
-      description: 'Página web desarrollada con VITE, utilizando Material-UI',
+      description: 'Página web desarrollada con Vite y Material-UI, enfocada en una experiencia moderna y eficiente.',
       url: 'https://eymlimitada.cl',
       image: 'https://firebasestorage.googleapis.com/v0/b/portfolio-9508a.appspot.com/o/eymlimitada.jpg?alt=media&token=f74e651b-c563-4587-9619-1b045ea56b1c',
     },
     {
       title: 'Proyecto WellnessApp',
-      description: 'Proyecto ecommerce creado con react, utilizando material-UI, react-router-dom, context, sweet alert y swiper. Conectado una base de datos en Firebase para la persistencia de datos.',
+      description: 'Ecommerce creado con React, utilizando Material-UI, React Router, Context, Sweet Alert y Swiper. Integra Firebase para la persistencia de datos.',
       url: 'https://github.com/maxzirox/WellnessApp',
       image: 'https://firebasestorage.googleapis.com/v0/b/wellnesgym-47cea.appspot.com/o/wellnesGym.gif?alt=media&token=ea58bc73-0c6e-4612-b47e-1d1eabeb941b',
+    },
+    {
+      title: 'Proyecto ecommerceApp',
+      description: 'Aplicación de ecommerce desarrollada en React Native, usando Material-UI, Redux, React Navigation, Image Picker y Swiper. Conexión a Firebase para almacenar datos.',
+      url: 'https://github.com/maxzirox/CoderApp',
+      image: 'https://firebasestorage.googleapis.com/v0/b/wellnesgym-47cea.appspot.com/o/kpc.jpg?alt=media&token=141eff94-498a-47fa-ac7c-3528e703ee86',
+    },
+    {
+      title: 'Mi canasta Express',
+      description: 'Proyecto web desarrollado con Vite, utilizando Tailwind CSS, React Router, EmailJS y Swiper para crear una experiencia fluida y funcional.',
+      url: 'https://github.com/maxzirox/Mi_Canasta_Expressp',
+      image: 'https://firebasestorage.googleapis.com/v0/b/portfolio-9508a.appspot.com/o/micanastaexpress.jpg?alt=media&token=a9af4ae1-8f9d-4871-bd5a-1ac6a018e897',
     },
     // Agrega más proyectos aquí
   ];
@@ -79,6 +91,7 @@ const ProjectsComponent = () => {
           marginTop: { xs: '90px', sm: '50px', md: '150px', lg: '100px' },
           overflowX: "auto", // Habilitar el desplazamiento horizontal
           whiteSpace: "wrap", // Asegura que las tarjetas se alineen en una línea
+          marginBottom: "50px"
         }}
       >
         <Grid container spacing={3} sx={{ marginTop: "20px", display: "inline-flex" }}>
